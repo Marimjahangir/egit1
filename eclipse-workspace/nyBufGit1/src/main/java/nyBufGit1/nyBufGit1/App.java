777748bf4524adb06f42db14b92fff158864fa2c
mariamjahangir <momodh2@yahoo.com>
2021-12-27 17:30:41 -0500

    initial commit of my maven project

---
+++ eclipse-workspace/nyBufGit1/src/main/java/nyBufGit1/nyBufGit1/App.java
@@ -0,0 +1,13 @@
+package nyBufGit1.nyBufGit1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
